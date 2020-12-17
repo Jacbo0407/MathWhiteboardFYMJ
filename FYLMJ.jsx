596fe6ee5fb66007a60ee23075f5fe52abacb02e
Jacbo0407 <76184218+Jacbo0407@users.noreply.github.com>
2020-12-17 15:07:26 -0600

    Fml
    Fml

---
+++ FYLMJ.jsx
@@ -0,0 +1,55 @@
+import './App.css';
+import React, { useRef, useEffect, useState } from 'react';
+//import Container from './components/container/Container';
+
+export default class App extends React.Component() {
+  constructor() {
+    let canvasRef = useRef(null)
+    let contextRef = useRef(null)
+    let[isDrawing, setIsDrawing] = useState(false)
+
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth * 2;
+    canvas.height = window.innerHeight * 2;
+    canvas.style.width = `${window.innerWidth}px`;
+    canvas.style.height = `${window.innerHeight}px`;
+
+    const context = canvas.getContext("2d");
+    context.scale(2,2);
+    context.lineCap = "round"
+    context.strokeStyle = "black"
+    context.lineWidth = 5
+    contextRef.current = context;
+  }, [])
+
+  const startDrawing = () => {
+    const {offsetX, offsetY} = nativeEvent;
+    contextRef.current.beginPath()
+    contextRef.current.moveTo(offsetX, offsetY)
+    setIsDrawing(true)
+  }
+  const finishDrawing = () => {
+    contextRef.current.closePath()
+    setIsDrawing(false)
+  }
+  const draw = () => {
+    if(!isDrawing){
+      return;
+    }
+    const {offsetX, offsetY} = nativeEvent;
+    contextRef.current.lineTo(offsetX, offsetY)
+    contextRef.current.stroke()
+    
+  }
+  }
+  render() {
+    return (<canvas onMouseDown={startDrawing}
+      onMouseDown={finishDrawing}
+      onMouseDown={draw}
+      ref={canvasRef}
+      />
+    );
+  }
+}
